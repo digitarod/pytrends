5601f9d5614cac9900c8f1d22fd3c0addd742979
digitarod <digitarod@gmail.com>
2025-02-27 22:57:34 +0900

    Update trends.py

--- trends.py
+++ trends.py
@@ -4,7 +4,8 @@ import json
 
 # Googleトレンドに接続
 pytrends = TrendReq(hl='ja-JP', tz=540)
-trending_searches_df = pytrends.trending_searches(pn='japan')
+#trending_searches_df = pytrends.trending_searches(pn='japan')
+trending_searches_df = pytrends.trending_searches(pn='united_states')
 
 # 現在の日付を取得
 today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
